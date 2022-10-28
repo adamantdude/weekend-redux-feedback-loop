22584985bb2c141684285fa1142c65ac62cd8410
adamantdude <adam.plee23@gmail.com>
2022-10-28 10:51:39 -0500

    set connection to database in server.js

---
+++ server/routes/feedback.router.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool');
+
+// router.get('/', (req, res) => {
+
+// })
+
+// router.post('/', (req, res) => {
+
+// })
+
+// router.put('/', (req, res) => {
+
+// })
+
+// router.delete('/', (req, res) => {
+
+// })
+
+module.exports = router;
